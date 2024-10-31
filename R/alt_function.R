d4f01e693b1d935359b702090a7f3de98d8e89e1
Joshua Martin <joshua.martin-3@selu.edu>
2024-10-31 19:06:14 +0000

    Added classwork 10-31-24

---
+++ R/alt_function.R
@@ -0,0 +1,9 @@
+alt_function <- function(path, inputone, inputtwo){
+  dataframe <- read_csv(path)
+  dataframe
+  dataframe %>%
+    na.omit(dataframe) %>%
+    mutate(useless = as.double(inputone) * as.double(inputtwo))
+
+  return(dataframe)
+}
--- R/practice_function.R
+++ R/practice_function.R
@@ -1,10 +1,17 @@
+#' Takes in a csv database and squares the values of a numeric column specified
+#'
+#' @param filepath The path to the csv file you want to process
+#' @param clmns The column of values you wish to get squared values for
+#'
+#' @return sqrd the original dataset with a new column of squared values.
+#' @export
 practice_function <- function(filepath, clmns){
-  dataframe <- read_csv(filepath)
-  
+  dataframe <- readr::read_csv(filepath)
+
   dataframe <- dataframe %>%
-    na.omit(dataframe) %>% 
-    pull({{clmns}})
-  
+    na.omit(dataframe) %>%
+    dplyr::pull({{clmns}})
+
   if(is.numeric(dataframe)){
     ##clmns references weight variable instead of dataframe index.
     message(clmns)
