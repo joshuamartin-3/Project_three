591084a48ad41d4ceb697160776d63eafbf7596f
Joshua Martin <joshua.martin-3@selu.edu>
2024-11-21 21:06:39 +0000

    Fixed error with corrupted package underwent inital testing of buildsite(pkgdown).

--- R/survey_dotplot.R
+++ R/survey_dotplot.R
@@ -3,12 +3,11 @@
 #' @param filepath The path to the csv file you want to process
 #' @param x_var the independent varaible infuencing the y-axis variable
 #' @param y_var the dependent variable varying in response to the x-axis variable
-#' @param color_group
 #'
 #' @return survey_plot the scatterplot using the surveys dataset comparing two variables x_var and y_var, grouping the two variables by color_group
 #' @export
 
-survey_dotplot <- function(filepath, x_var, y_var, color_group){
+survey_dotplot <- function(filepath, x_var, y_var){
   dataframe <- survey_cleaner(filepath)
 
   survey_plot <- ggplot2::ggplot(dataframe, aes(x = {{x_var}}, y = {{y_var}}, color = {{color_group}})) + geom_point(size = 2) + labs(x = var_one, y = var_two) + theme(legend.position = "right")
